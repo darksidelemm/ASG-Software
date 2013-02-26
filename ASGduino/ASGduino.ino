09d1e4560bd5e23050c157bb9af502f6a56903a0
Mark Jessop <darkside@staff-249-004.wireless.adelaide.edu.au>
2013-02-26 15:57:15 +1030

    Added up/down support. Use ^ and v.

--- ASGduino/ASGduino.ino
+++ ASGduino/ASGduino.ino
@@ -34,6 +34,8 @@
 #define FIRE  2
 #define AZ_DIR  9
 #define AZ_STEP 8
+#define ELE_DIR 4
+#define ELE_STEP 3
 #define LED    13
 #define POT_INPUT   5
 
@@ -42,16 +44,25 @@
 
 #define TIMER_PERIOD  300
 unsigned int fire_timer = 0;
-unsigned int step_timer = 0;
+unsigned int az_timer = 0;
+unsigned int ele_timer = 0;
 
 void asg_isr(){
    if(fire_timer>0) fire_timer--;
-   if(step_timer>0) step_timer--; 
+   if(az_timer>0) az_timer--;
+   if(ele_timer>0) ele_timer--; 
    
    if(fire_timer==0) digitalWrite(FIRE, LOW);
-   if(step_timer&1 == 1) digitalWrite(AZ_STEP, HIGH);
-   if(step_timer&0 == 0) digitalWrite(AZ_STEP, LOW);
-   if(step_timer==0 && fire_timer==0) digitalWrite(LED, LOW);
+   if(az_timer>0){
+     if(az_timer&1 == 1) digitalWrite(AZ_STEP, HIGH);
+     if(az_timer&0 == 0) digitalWrite(AZ_STEP, LOW);
+    }
+    
+    if(ele_timer>0){
+        if(ele_timer&1 == 1) digitalWrite(ELE_STEP, HIGH);
+        if(ele_timer&0 == 0) digitalWrite(ELE_STEP, LOW);
+    }
+   if(az_timer==0 && fire_timer==0) digitalWrite(LED, LOW);
 }
 
 void right(){
@@ -62,9 +73,24 @@ void left(){
   digitalWrite(AZ_DIR, LOW);
 }
 
-void step(){
+void az_step(){
   Timer1.detachInterrupt();
-  step_timer = step_timer + STEP_UNIT;
+  az_timer = az_timer + STEP_UNIT;
+  digitalWrite(LED, HIGH);
+  Timer1.attachInterrupt(asg_isr);
+}
+
+void up(){
+  digitalWrite(ELE_DIR, HIGH);
+}
+
+void down(){
+  digitalWrite(ELE_DIR, LOW);
+}
+
+void ele_step(){
+  Timer1.detachInterrupt();
+  ele_timer = ele_timer + STEP_UNIT;
   digitalWrite(LED, HIGH);
   Timer1.attachInterrupt(asg_isr);
 }
@@ -72,7 +98,8 @@ void step(){
 void stop(){
   Timer1.detachInterrupt();
   fire_timer = 0;
-  step_timer = 0;
+  az_timer = 0;
+  ele_timer = 0;
   Timer1.attachInterrupt(asg_isr);
 }
 
@@ -89,6 +116,9 @@ void setup(){
   digitalWrite(FIRE,LOW);
   pinMode(AZ_DIR, OUTPUT);
   pinMode(AZ_STEP, OUTPUT);
+  pinMode(ELE_DIR, OUTPUT);
+  pinMode(ELE_STEP, OUTPUT);
+  digitalWrite(ELE_DIR, LOW);
   digitalWrite(AZ_DIR, LOW);
   digitalWrite(AZ_DIR, LOW);
   pinMode(LED, OUTPUT);
@@ -107,10 +137,14 @@ void loop(){
       
       if(incomingByte == '<'){
         //Serial.println("OK");
-        left(); step();
+        left(); az_step();
       }else if(incomingByte == '>'){
         //Serial.println("OK");
-        right(); step();
+        right(); az_step();
+      }else if(incomingByte == '^'){
+        up(); ele_step();
+      }else if(incomingByte == 'v'){
+        down(); ele_step();
       }else if(incomingByte == 'f'){
         //Serial.println("OK");
          fire();
